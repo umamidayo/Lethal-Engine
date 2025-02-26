df9655d1f6fd1d735b8c0157370a3f4cd5743f9e
umamidayo <raymond.nguyen96@gmail.com>
2025-02-26 04:53:32 -0600

    remove acs

--- src/ReplicatedStorage/ACS_Engine/Modules/ACSModifier.lua
+++
@@ -1,6 +0,0 @@
-local module = {
-	TracerTweenInfo = TweenInfo.new(5, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut),
-	BulletDropForceTweenInfo = TweenInfo.new(1.2, Enum.EasingStyle.Linear)
-}
-
-return module
